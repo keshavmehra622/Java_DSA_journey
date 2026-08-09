586cff3c5a72fa59f25cf539548da3f8b9090a41
Keshav Mehra <keshavmehra622@gmail.com>
2026-08-09 23:16:45 +0530

    added one line of code

--- Day 2/conditions.java
+++ Day 2/conditions.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class conditions {
     public static void main(String[] args) {
       Scanner sc =  new Scanner(System.in);
+      System.out.println("Hello world");
 
       // Conditions : if , if-else , if-else-if 
 
